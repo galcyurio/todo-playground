170d6cd36ccf260c91d4ab211a4811b01a942fc0
galcyurio <galcyurio@gmail.com>
2020-11-01 22:24:54 +0900

    kotlin 업그레이드 (1.3.72 -> 1.4.10)

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -2,7 +2,7 @@ object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.0"
 
     object Kotlin {
-        private const val version = "1.3.72"
+        private const val version = "1.4.10"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
     }
