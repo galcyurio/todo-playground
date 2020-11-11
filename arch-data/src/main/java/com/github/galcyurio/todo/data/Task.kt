2ceb4ed80d5d9de7eac179bff9429aca9dddfff7
galcyurio <galcyurio@gmail.com>
2020-11-12 00:20:18 +0900

    AppDatabase와 Task 클래스 구현 (#17)

---
+++ arch-data/src/main/java/com/github/galcyurio/todo/data/Task.kt
@@ -0,0 +1,12 @@
+package com.github.galcyurio.todo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    val title: String,
+    val description: String,
+    val isCompleted: Boolean,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0
+)
