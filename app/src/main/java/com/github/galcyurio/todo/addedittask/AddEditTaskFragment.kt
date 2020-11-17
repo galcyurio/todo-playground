37bc3372b90a4268044010f0e789fdf2ba728e8a
galcyurio <galcyurio@gmail.com>
2020-11-17 09:27:35 +0900

    스켈레톤 AddEditTaskFragment 추가 (#19)

---
+++ app/src/main/java/com/github/galcyurio/todo/addedittask/AddEditTaskFragment.kt
@@ -0,0 +1,28 @@
+package com.github.galcyurio.todo.addedittask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.github.galcyurio.todo.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddEditTaskFragment : Fragment() {
+    private val viewModel: AddEditTaskViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.add_edit_task_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/github/galcyurio/todo/addedittask/AddEditTaskViewModel.kt
@@ -0,0 +1,8 @@
+package com.github.galcyurio.todo.addedittask
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class AddEditTaskViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
