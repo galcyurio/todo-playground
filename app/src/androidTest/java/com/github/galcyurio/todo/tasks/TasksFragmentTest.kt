6e5aa07f924b1d6ff305f49411742edaaa6c59d1
galcyurio <galcyurio@gmail.com>
2020-11-19 00:00:04 +0900

    각 테스트마다 launchActivity() 대신 ActivityScenarioRule 활용 (#19)

--- app/src/androidTest/java/com/github/galcyurio/todo/tasks/TasksFragmentTest.kt
+++ app/src/androidTest/java/com/github/galcyurio/todo/tasks/TasksFragmentTest.kt
@@ -3,7 +3,7 @@ package com.github.galcyurio.todo.tasks
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.galcyurio.todo.MainActivity
 import com.github.galcyurio.todo.R
@@ -11,6 +11,7 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.hamcrest.Matchers.equalTo
 import org.junit.Assert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,17 +20,20 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TasksFragmentTest {
     @get:Rule val hiltRule = HiltAndroidRule(this)
+    @get:Rule val activityScenarioRule = activityScenarioRule<MainActivity>()
 
-    @Test fun showTasksUi() {
-        // given
-        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+    private lateinit var navController: TestNavHostController
+
+    @Before fun setUp() {
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
         navController.setGraph(R.navigation.nav_graph)
-
-        // when - 화면이 뜨면
-        launchActivity<MainActivity>().onActivity {
+        activityScenarioRule.scenario.onActivity {
             Navigation.setViewNavController(it.requireViewById(R.id.nav_host_fragment), navController)
         }
+    }
 
+    @Test fun showTasksUi() {
+        // when - 화면이 뜨면
         // then - 업무 목록 화면이 보여야 한다
         assertThat(navController.currentDestination?.id, equalTo(R.id.tasksFragment))
     }
