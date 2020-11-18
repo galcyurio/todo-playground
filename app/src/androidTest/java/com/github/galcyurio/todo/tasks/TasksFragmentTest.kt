9018fc3be6318af9df6be2e0445087b459b0a258
galcyurio <galcyurio@gmail.com>
2020-11-18 23:38:34 +0900

    tasks_fragment.xml에서 id를 제거하고 TestNavHostController 활용하여 테스트 코드 수정 (#19)

--- app/src/androidTest/java/com/github/galcyurio/todo/tasks/TasksFragmentTest.kt
+++ app/src/androidTest/java/com/github/galcyurio/todo/tasks/TasksFragmentTest.kt
@@ -1,15 +1,16 @@
 package com.github.galcyurio.todo.tasks
 
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.galcyurio.todo.MainActivity
 import com.github.galcyurio.todo.R
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,10 +21,16 @@ class TasksFragmentTest {
     @get:Rule val hiltRule = HiltAndroidRule(this)
 
     @Test fun showTasksUi() {
+        // given
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        navController.setGraph(R.navigation.nav_graph)
+
         // when - 화면이 뜨면
-        launchActivity<MainActivity>()
+        launchActivity<MainActivity>().onActivity {
+            Navigation.setViewNavController(it.requireViewById(R.id.nav_host_fragment), navController)
+        }
 
         // then - 업무 목록 화면이 보여야 한다
-        onView(withId(R.id.tasksFragment)).check(matches(isDisplayed()))
+        assertThat(navController.currentDestination?.id, equalTo(R.id.tasksFragment))
     }
 }
