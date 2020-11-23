60c4898a63cf3dbfb3c3d70980fe62d48ce23e3f
galcyurio <galcyurio@gmail.com>
2020-11-24 00:03:35 +0900

    업무 수정화면에 메뉴 추가 (#20)

--- app/src/main/java/com/github/galcyurio/todo/addedittask/AddEditTaskFragment.kt
+++ app/src/main/java/com/github/galcyurio/todo/addedittask/AddEditTaskFragment.kt
@@ -1,9 +1,7 @@
 package com.github.galcyurio.todo.addedittask
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.galcyurio.todo.R
@@ -17,8 +15,24 @@ class AddEditTaskFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.add_edit_task_fragment, container, false)
     }
+
+    //region Menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.add_edit_task_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+        R.id.menu_save -> {
+            TODO("업무 저장")
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+    //endregion
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
