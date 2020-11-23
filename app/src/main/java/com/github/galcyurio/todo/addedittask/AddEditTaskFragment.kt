5a1125e422f89c702fa713bcaa821c0acbec944f
galcyurio <galcyurio@gmail.com>
2020-11-24 00:12:04 +0900

    AddEditTaskFragment 데이터바인딩 적용 (#20)

--- app/src/main/java/com/github/galcyurio/todo/addedittask/AddEditTaskFragment.kt
+++ app/src/main/java/com/github/galcyurio/todo/addedittask/AddEditTaskFragment.kt
@@ -5,18 +5,27 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.galcyurio.todo.R
+import com.github.galcyurio.todo.databinding.AddEditTaskFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AddEditTaskFragment : Fragment() {
+    private var _binding: AddEditTaskFragmentBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: AddEditTaskViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = AddEditTaskFragmentBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.add_edit_task_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     //region Menu
@@ -25,7 +34,7 @@ class AddEditTaskFragment : Fragment() {
         inflater.inflate(R.menu.add_edit_task_menu, menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         R.id.menu_save -> {
             TODO("업무 저장")
             true
